b0f76e235a66906a58c2a8d37d95951323075905
Loicwr <nephtyse19@hotmail.fr>
2025-05-09 15:43:41 +0200

    Refactor exercise comments for clarity and consistency

--- exo.js
+++ exo.js
@@ -11,27 +11,33 @@ else {
 
       // Exercices jour 5 
 
+    /*Exercice 1
+    Affichez un "Hello World !"*/
+
     let value = "Hello world ";
     alert (value)
 
-    //
+    /*Exercice 2
+    Créez une variable Age Affichez le contenu de cette variable*/
 
     let age = 19;
     alert ( age )
 
-    //
+    /*Exercice 3
+    Demandez à l'utilisateur de saisir son âge Affichez le contenu de la valeur saisie*/
     
     let ageSaisir = prompt ("Quel est votre âge ?")
     alert (ageSaisir);
 
-    //
+    /*Exercice 4
+    Demandez à l'utilisateur de saisir le rayon d'un cercle Affichez le périmètre du cercle*/
 
     let rayonCercle = prompt ("Quel est le rayon du cercle ?")
     resultat = 3.14 * 2 * rayonCercle
     alert ("Le périmètre du cercle est de : " + resultat)
 
-    //
-
+    /*Exercice 5
+    Soit 2 valeurs saisies par une utilisateur dans 2 variables Inversez ces variables*/
     let a = 5;
     let b = 10;
 
