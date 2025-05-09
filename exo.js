e2a9c33d952d75e7f0eaa15624e09968a700c4ec
Loicwr <nephtyse19@hotmail.fr>
2025-05-09 14:05:37 +0200

    Add commented-out JavaScript code for ECMAScript quiz

---
+++ exo.js
@@ -0,0 +1,11 @@
+/*
+let value = prompt ("Quel est le nom officiel de Javascript")
+
+if (value.toLowerCase == "ECMAScript") {
+    console.log (" bonne réponse ! ")
+}
+else {
+    console.log (" Tu ne connais pas ECMAScript ?")
+} */
+
+    
